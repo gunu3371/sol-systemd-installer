4e96d8972f412482852273cc53d6e0f0f6031e7a
gunu3371 <55932910+gunu3371@users.noreply.github.com>
2025-02-12 00:27:07 +0900

    Update install_sol.sh

--- install_sol.sh
+++ install_sol.sh
@@ -24,7 +24,7 @@ echo """{
           },
           {
               "Operation": "sleep",
-              "Command": "suspend",
+              "Command": "systemctl suspend",
               "Default": true,
               "Type": "external"
           }
